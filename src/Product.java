a9884a2826de23b55b80b62b439f174a37cba555
Saniya Ruey <saniyaruey@gmail.com>
2026-06-25 10:36:43 +0530

    Product code

---
+++ src/Product.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Product {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:  ");
+        int n=sc.nextInt();
+        int product=1;
+
+        for(int i=1;i<=n;i++){
+            product = product*i;
+            System.out.println(product);
+        }
+        sc.close();
+    }
+}
