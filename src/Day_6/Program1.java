70cc890e66d28606673c8b0a6d33daf34af6578b
Saniya Ruey <saniyaruey@gmail.com>
2026-06-25 11:58:16 +0530

    Program1 code

---
+++ src/Day_6/Program1.java
@@ -0,0 +1,13 @@
+package Day_6;
+import java.util.Scanner;
+public class Program1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
