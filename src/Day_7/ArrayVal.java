23c15469235389f6960f9dd2b25d8cd2e55e7469
Saniya Ruey <saniyaruey@gmail.com>
2026-06-27 10:16:47 +0530

    ArrayVal code

---
+++ src/Day_7/ArrayVal.java
@@ -0,0 +1,16 @@
+package Day_7;
+import java.util.Scanner;
+public class ArrayVal {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int [] arr=new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+        arr[4]=5;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);}
+    }
+
+}
