98c5d6fffa0c08bf3c3da8127d04845c70b53a15
Saniya Ruey <saniyaruey@gmail.com>
2026-06-29 12:14:31 +0530

    ArrayRotation code

--- src/Day8/ArrayRotation.java
+++ src/Day8/ArrayRotation.java
@@ -1,4 +1,21 @@
 package Day8;
 
 public class ArrayRotation {
+    public static void main(String [] args){
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+
+        System.out.println("Array after right rotation:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
