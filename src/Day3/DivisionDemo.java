56e16a3d3b727edcf8671ba54ba3160eff971fab
Saniya Ruey <saniyaruey@gmail.com>
2026-06-30 10:37:27 +0530

    DivisionDemo code

---
+++ src/Day3/DivisionDemo.java
@@ -0,0 +1,9 @@
+package Day3;
+
+public class DivisionDemo {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+        System.out.println("Integer Division = " + (a / b));
+        System.out.println("Floating Division = " + ((double)a / b));
+    }
+}
