f32794a9ea9e9bfec2df2eab026d6ee95d1dc919
Saniya Ruey <saniyaruey@gmail.com>
2026-07-04 10:19:47 +0530

    Error code

---
+++ src/Day13/Error.java
@@ -0,0 +1,17 @@
+package Day13;
+import java.util.Scanner;
+public class Error {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        try{
+            int a=sc.nextInt();
+            System.out.print(a+a);
+        }
+        catch(Exception e){
+
+        }
+       System.out.println("Program ended");h
+
+    }
+}
